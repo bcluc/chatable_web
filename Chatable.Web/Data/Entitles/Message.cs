4b85437b1a6cb9fdaf01935381dbfc95f2093e55
bcluc <21521898@gm.uit.edu.vn>
2023-12-11 20:23:43 +0700

    fix and add model

---
+++ Chatable.Web/Data/Entitles/Message.cs
@@ -0,0 +1,24 @@
+﻿namespace Chatable.Web.Data.Entitles
+{
+    public class Message
+    {
+        public string MessageId { get; set; }
+        public string SenderId { get; set; }
+        public string ConversationId { get; set; }
+        public DateTime SendAt { get; set; }
+        public string MessageType { get; set; }
+
+        public string Content { get; set; }
+        public Message() { }
+
+        public Message(string messageId, string senderId, string conversationId, DateTime sendAt, string messageType, string content)
+        {
+            MessageId = messageId;
+            SenderId = senderId;
+            ConversationId = conversationId;
+            SendAt = sendAt;
+            MessageType = messageType;
+            Content = content;
+        }
+    }
+}
---
+++ Chatable.Web/Data/Entitles/User.cs
@@ -0,0 +1,22 @@
+﻿namespace Chatable.Web.Data.Entitles
+{
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Avatar { get; set; }
+        public string LastTimeOnline { get; set; }
+        public User() { }
+        public User(string userName, string fullName, string avatar, string lastTimeOnline)
+        {
+            UserName = userName;
+
+            FullName = fullName;
+            Avatar = avatar;
+            LastTimeOnline = lastTimeOnline;
+        }
+        public bool IsSelected { get; set; }
+    }
+}
