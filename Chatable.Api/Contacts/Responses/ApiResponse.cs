630a67367f83ff58db1cbf8e233f3c1d86a2e5cb
petrichor-hl <tl.hoanglam174@gmail.com>
2023-12-15 09:05:05 +0700

    continue

---
+++ Chatable.Api/Contacts/Responses/ApiResponse.cs
@@ -0,0 +1,9 @@
+namespace Chatable.Api.Contacts.Responses
+{
+    public class ApiResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
